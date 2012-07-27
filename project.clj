dfbf3b864f6a2acce25f40d6c23815d9f6d4d380
Ben Smith-Mannschott <bsmith.occs@gmail.com>
2012-07-27 08:03:28 +0200

    exclude xml-apis, xercesImpl, xalan
    (I believe that) these are not required on modern JVMs.

--- project.clj
+++ project.clj
@@ -2,7 +2,9 @@
   :description "Teaching Clojure to love the XOM"
   :url "http://github.com/bpsm/xombie"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [xom "1.2.5"]] ;; nothing newer available on maven central
+                 [xom "1.2.5" :exclusions [xml-apis/xml-apis
+                                           xerces/xercesImpl
+                                           xalan/xalan]]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   )
